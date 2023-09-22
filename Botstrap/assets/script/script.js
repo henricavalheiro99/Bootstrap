0c9494d1f8616be90419f1c0329d6e1bb556437e
henricavalheiro99 <124930679+henricavalheiro99@users.noreply.github.com>
2023-09-22 15:13:51 -0300

    Continuando o forms

--- Botstrap/assets/script/script.js
+++ Botstrap/assets/script/script.js
@@ -9,12 +9,12 @@ setInterval(() => {
 var title = ['Hamburguer', 'Combos']
 var subtitle = ['do tamanho da sua fome', 'para toda família']
     for(let quant=0; quant<title.length; quant++){
-        document.querySelector('.cidades-desconto').innerHTML += `         
-                <div style="background-image: url(assets/img/desconto/cidade${quant}.png);" class="cidade">
+        document.querySelector('.duas-imgs').innerHTML += `         
+                <div style="background-image: url(assets/img/BaianagemBanner/${quant}.jpeg);" class="img-baianagem">
                     <div class="details">
                         <div class="detail-box">
-                            <h3>${descontos[quant]}</h3>
-                            <h3>R$${preco[quant]},00</h3>
+                            <h3 class="title-baianagem">${title[quant]}</h3>
+                            <h3 class="subtitle-baianagem">${subtitle[quant]}</h3>
                             <button class="vermais">Ver Mais</button>
                         </div>
                     </div>
